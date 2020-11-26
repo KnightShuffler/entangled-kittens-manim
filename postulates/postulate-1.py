db917ccd6e01c339dc5d5420d4e2e0fd58b78c6e
Swagat <swagat.ks.97@gmail.com>
2020-11-25 23:51:08 -0600

    updated to run on manim community edition

--- postulates/postulate-1.py
+++ postulates/postulate-1.py
@@ -1,20 +1,20 @@
-from manimlib.imports import *
+from manim import *
 
 class Postulate1_Statement(Scene):
     def construct(self):
-        title = TextMobject(r'\textbf{Postulate 1:}').move_to(LEFT_SIDE + 2*UP,aligned_edge=LEFT_SIDE)
+        title = Tex(r'\textbf{Postulate 1:}').move_to(7*LEFT+2*UP,aligned_edge=LEFT)
 
         statement = [
-                        TextMobject(r'Every '),
-                        TextMobject(r'isolated quantum system',color='yellow'),
-                        TextMobject(r'has an associated'),
-                        TextMobject(r"``Hilbert Space''",color='yellow'), 
-                        TextMobject(r' called the'),
-                        TextMobject(r'\textit{state space}',color='yellow'),
-                        TextMobject(r' of that system. The system is completely'),
-                        TextMobject(r'described by the '),
-                        TextMobject(r'\textit{state vector,}',color='yellow'),
-                        TextMobject(r' a unit vector in the state space.')
+                        Tex(r'Every '),
+                        Tex(r'isolated quantum system',color='yellow'),
+                        Tex(r'has an associated'),
+                        Tex(r"``Hilbert Space''",color='yellow'), 
+                        Tex(r' called the'),
+                        Tex(r'\textit{state space}',color='yellow'),
+                        Tex(r' of that system. The system is completely'),
+                        Tex(r'described by the '),
+                        Tex(r'\textit{state vector,}',color='yellow'),
+                        Tex(r' a unit vector in the state space.')
         ]
         isolated_quantum_system = statement[1]
         hilbert_space = statement[3]
@@ -22,15 +22,15 @@ class Postulate1_Statement(Scene):
         state_vector = statement[8]
         postulate = Group(title,*statement)
         for s in statement:
-            s.scale(0.75)
+            s.scale(0.70)
         statement[0].next_to(title)
         statement[1].next_to(statement[0])
         statement[2].next_to(statement[1],direction=0.8*RIGHT)
         statement[3].next_to(statement[2],direction=0.8*RIGHT)
-        statement[4].next_to(statement[0],direction=DOWN,aligned_edge=LEFT_SIDE)
+        statement[4].next_to(statement[0],direction=DOWN,aligned_edge=LEFT)
         statement[5].next_to(statement[4],direction=0.8*RIGHT)
         statement[6].next_to(statement[5],direction=0.8*RIGHT)
-        statement[7].next_to(statement[4],direction=DOWN,aligned_edge=LEFT_SIDE)
+        statement[7].next_to(statement[4],direction=DOWN,aligned_edge=LEFT)
         statement[8].next_to(statement[7],direction=0.8*RIGHT)
         statement[9].next_to(statement[8],direction=0.8*RIGHT)
         
@@ -63,7 +63,7 @@ class Postulate1_Statement(Scene):
     def highlight_and_explain(self, base, definition):
         title = base.copy()
         self.play(ApplyMethod(title.move_to,ORIGIN), ApplyMethod(base.scale,1.1))
-        defn = TextMobject(definition).next_to(title,direction=DOWN)
+        defn = Tex(definition).next_to(title,direction=DOWN)
         self.play(Write(defn))
         self.wait(1)
         group = Group(title, defn)
